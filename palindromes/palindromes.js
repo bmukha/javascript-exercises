0a33473f89c15647fea0a20b4d7e24ebfdb21c83
Bohdan Mukha <by.muha@gmail.com>
2020-10-05 00:41:44 +0300

    palindromes done

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function() {
+const palindromes = function (string) {
+  const reg = /[a-zA-Z]+/;
+  const arr = string
+    .toLowerCase()
+    .split("")
+    .filter((item) => item.match(reg));
+  const rev = [...arr].reverse();
+  return JSON.stringify(arr) === JSON.stringify(rev);
+};
 
-}
-
-module.exports = palindromes
+module.exports = palindromes;

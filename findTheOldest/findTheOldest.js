58ff5cd34b46d3d998d9e393fe7384ac7309c894
Bohdan Mukha <by.muha@gmail.com>
2020-10-10 01:09:50 +0300

    findTheOldest done

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-let findTheOldest = function() {
+// name: "Ray",
+//         yearOfBirth: 1962,
+//         yearOfDeath: 2011,
 
-}
+// function compare (person1, person2)
 
-module.exports = findTheOldest
+let findTheOldest = function (arr) {
+  oldest = arr[0];
+  if (!oldest.yearOfDeath) {
+    oldest.yearOfDeath = new Date().getFullYear();
+  }
+
+  arr.reduce(function (acc, person) {
+    if (!person.hasOwnProperty("yearOfDeath")) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+    if (
+      person.yearOfDeath - person.yearOfBirth >
+      oldest.yearOfDeath - oldest.yearOfBirth
+    ) {
+      oldest = person;
+    }
+  });
+  return oldest;
+};
+
+module.exports = findTheOldest;

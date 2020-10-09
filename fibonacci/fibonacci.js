79df02877ab03838b7d3dc9220adc5fb3fac3986
Bohdan Mukha <by.muha@gmail.com>
2020-10-10 00:10:26 +0300

    fibonacci done

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (value) {
+  if (value < 0) {
+    return "OOPS";
+  } else if (value == 1 || value == 2) {
+    return 1;
+  } else {
+    sum = 0;
+    first = 1;
+    second = 1;
+    for (let i = 0; i < value - 2; i++) {
+      sum = first + second;
+      first = second;
+      second = sum;
+    }
+  }
+  return sum;
+};
 
-}
-
-module.exports = fibonacci
+module.exports = fibonacci;
